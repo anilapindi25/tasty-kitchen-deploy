92743d122b0b499bc8d0208e17f25db5790281e2
anilapindi25 <2024eb01539@online.bits-pilani.ac.in>
2025-08-31 13:16:09 +0530

    Fix: corrected Login import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,10 @@
-// import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// import Login from './components/login'
-// import Home from './components/Home'
-// import Products from './components/Products'
-// import Cart from './components/Cart'
-// import ProtectedRoute from './components/ProtectedRoute'
-// import ProductItemDetails from './components/ProductItemDetails'
-// import { CartProvider } from './context/CartContext' 
-// import PaymentSuccess from './components/PaymentSuccess'
-// import PageNotFound from './components/PageNotFound'
-// import './App.css'
 
-// const App = () => (
-//   <BrowserRouter>
-//     <CartProvider>
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         <Route
-//           path="/"
-//           element={
-//             <ProtectedRoute>
-//               <Home />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/products"
-//           element={
-//             <ProtectedRoute>
-//               <Products />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/restaurants/:restrauntId"
-//           element={
-//             <ProtectedRoute>
-//               <ProductItemDetails />
-//             </ProtectedRoute>
-//           }
-// />
-//         <Route
-//           path="/cart"
-//           element={
-//             <ProtectedRoute>
-//               <Cart />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/payment-success"
-//           element={
-//             <ProtectedRoute>
-//               <PaymentSuccess />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route path="*" element={<PageNotFound />} />
-//       </Routes>
-//     </CartProvider>
-//   </BrowserRouter>
-// )
-
-// export default App
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 import { CartProvider } from './context/CartContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
-const Login = lazy(() => import('./components/Login'))
+const Login = lazy(() => import('./components/login'))
 const Home = lazy(() => import('./components/Home'))
 const Products = lazy(() => import('./components/Products'))
 const Cart = lazy(() => import('./components/Cart'))
